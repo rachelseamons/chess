1fc16b7b4f0adb8d242d773bf7341a476ba26be7
rachelseamons <rachelseamonshcp@gmail.com>
2024-02-25 12:14:36 -0700

    created User class

---
+++ server/src/main/java/dataAccess/User.java
@@ -0,0 +1,27 @@
+package dataAccess;
+
+public class User {
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean verifyPassword(String password) {
+        return password.equals(this.password);
+    }
+}
