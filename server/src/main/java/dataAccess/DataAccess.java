6a9077ee46152c0ddf01e374b9646db5efe50d34
rachelseamons <rachelseamonshcp@gmail.com>
2024-02-25 12:28:34 -0700

    first draft of DataAccess interface

--- server/src/main/java/dataAccess/DataAccess.java
+++ server/src/main/java/dataAccess/DataAccess.java
@@ -1,5 +1,20 @@
 package dataAccess;
 
+import javax.xml.crypto.Data;
+import java.util.Collection;
+
 public interface DataAccess {
-    
+    void clear() throws DataAccessException;
+
+    String registerUser(User user) throws DataAccessException;
+
+    String login(String username, String password) throws DataAccessException;
+
+    boolean logout(String authToken) throws DataAccessException;
+
+    Collection<String> listGames(String authToken) throws DataAccessException;
+
+    String createGame(String authToken, String gameName) throws DataAccessException;
+
+    String joinGame(String authToken, String playerColor, String gameID) throws DataAccessException;
 }
