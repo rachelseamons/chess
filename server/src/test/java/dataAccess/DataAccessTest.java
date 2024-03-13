3f4f216fbfe46f0839b00d5f6eb98d2d5cab3089
rachelseamons <rachelseamonshcp@gmail.com>
2024-03-13 16:44:41 -0600

    passing all DataAccess tests so far

--- server/src/test/java/dataAccess/DataAccessTest.java
+++ server/src/test/java/dataAccess/DataAccessTest.java
@@ -1,20 +1,21 @@
 package dataAccess;
 
 import model.User;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import service.ChessService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataAccessTest {
     private DataAccess getDataAccess(Class<? extends DataAccess> databaseClass) throws DataAccessException {
         DataAccess db;
-        //when implementing the database, add an if statement here to choose which implementation you want to use
-        db = new DataAccessMemory();
+        if (databaseClass == DataAccessMemory.class) {
+            db = new DataAccessMemory();
+        } else {
+            //when implementing the SQL Database, change this to that implementation
+            db = new DataAccessMemory();
+        }
 
         return db;
     }
@@ -69,5 +70,14 @@ class DataAccessTest {
     void clear(Class<? extends DataAccess> dbClass) throws DataAccessException {
         var dataAccess = getDataAccess(dbClass);
 
+        var fred = new User("Fred", "pass", "@gmail");
+        var carl = new User("Carl", "pass", "@yahoo");
+        var alex = new User("Alex", "pass", "@gmail");
+
+        dataAccess.createUser(fred);
+        dataAccess.createUser(carl);
+        dataAccess.createUser(alex);
+
+        assertDoesNotThrow(dataAccess::clear);
     }
 }
