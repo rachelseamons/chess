58b5789976240a3fa70fce0b20d96ac591e3ca4f
rachelseamons <rachelseamonshcp@gmail.com>
2024-09-09 21:47:36 -0600

    implemented constructor, getPossibleMoves in ChessRules

--- shared/src/main/java/chess/ChessRules.java
+++ shared/src/main/java/chess/ChessRules.java
@@ -1,5 +1,34 @@
 package chess;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ChessRules {
-    
+    private final ChessBoard board;
+    private ChessPiece piece;
+    private boolean repeatable = true;
+
+    public ChessRules(ChessBoard board, ChessPiece piece) {
+        this.board = board;
+        this.piece = piece;
+    }
+
+    Set<ChessMove> getPossibleMoves(ChessPosition startPosition) {
+        if (piece.getPieceType() == ChessPiece.PieceType.PAWN) {
+            return movePawn(startPosition);
+        }
+        if (piece.getPieceType() == ChessPiece.PieceType.ROOK) {
+            return moveRook(startPosition);
+        }
+        if (piece.getPieceType() == ChessPiece.PieceType.KNIGHT) {
+            return moveKnight(startPosition);
+        }
+        if (piece.getPieceType() == ChessPiece.PieceType.BISHOP) {
+            return moveBishop(startPosition);
+        }
+        if (piece.getPieceType() == ChessPiece.PieceType.QUEEN || piece.getPieceType() == ChessPiece.PieceType.KING) {
+            return moveRoyalty(startPosition);
+        }
+        return new HashSet<>();
+    }
 }
