0fdcdbea5cd00bafb14083ea48337c888f5c5b4c
rachelseamons <rachelseamonshcp@gmail.com>
2024-09-29 00:31:16 -0600

    implemented board getter and setter

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -242,7 +242,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board = board;
     }
 
     /**
@@ -251,6 +251,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return board;
     }
 }
