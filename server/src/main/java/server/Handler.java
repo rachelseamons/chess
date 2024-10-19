2f90ddc1e8b09bf94088abd0e29469c207e45af4
rachelseamons <rachelseamonshcp@gmail.com>
2024-10-19 16:43:18 -0600

    extracting data from all requests except joinGame

--- server/src/main/java/server/Handler.java
+++ server/src/main/java/server/Handler.java
@@ -3,12 +3,16 @@ package server;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import dataaccess.DataAccessException;
+import model.GameData;
 import model.UserData;
 import service.AuthService;
 import service.GameService;
 import service.UserService;
 import spark.Request;
 import spark.Response;
+
+import java.util.Map;
+import java.util.Set;
 
 public class Handler {
     private UserService userService = new UserService();
@@ -20,37 +24,54 @@ public class Handler {
     }
 
     public Object joinGame(Request request, Response response) throws DataAccessException {
+        var authToken = request.headers("Authorization");
+        //TODO:: I have no clue how to get the info from the request, short of creating another record
         return new Gson().toJson("join game");
     }
 
     public Object createGame(Request request, Response response) throws DataAccessException {
+        var authToken = request.headers("Authorization");
+        var game = new Gson().fromJson(request.body(), GameData.class);
+
         return new Gson().toJson("create game");
     }
 
     public Object getGames(Request request, Response response) throws DataAccessException {
+        var authToken = request.headers("Authorization");
         return new Gson().toJson("get games");
     }
 
     public Object logoutUser(Request request, Response response) throws DataAccessException {
+        var authToken = request.headers("Authorization");
         return new Gson().toJson("logout");
     }
 
     public Object loginUser(Request request, Response response) throws DataAccessException {
-        return new Gson().toJson("login");
+        var user = new Gson().fromJson(request.body(), UserData.class);
+
+        if (user.username() == null || user.password() == null) {
+            System.out.println("in fail 1");
+            response.status(400);
+            var message = "Error: bad request";
+            return new Gson().toJson(message);
+        }
+
+        return new Gson().toJson(user);
     }
 
     public Object registerUser(Request request, Response response) throws DataAccessException {
         var user = new Gson().fromJson(request.body(), UserData.class);
-        //TODO:: add try-catch block to handle syntax errors in requests
-        var confirmation = userService.registerUser(user);
 
         if (user.username() == null || user.password() == null || user.email() == null) {
             System.out.println("in fail 1");
             response.status(400);
             var message = "Error: bad request";
             return new Gson().toJson(message);
-        } else {
-            return new Gson().toJson(confirmation);
         }
+
+        //TODO:: add try-catch block to handle syntax errors in requests
+        // actually specs say to do that in the server class, so GAH
+        var confirmation = userService.registerUser(user);
+        return new Gson().toJson(confirmation);
     }
 }
