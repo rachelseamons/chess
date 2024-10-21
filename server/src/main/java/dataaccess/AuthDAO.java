f38865f1ad5f22671c7d99e9c405f729a0d59828
rachelseamons <rachelseamonshcp@gmail.com>
2024-10-21 10:14:43 -0600

    implemented createAuth in AuthMemoryDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -3,5 +3,5 @@ package dataaccess;
 import model.AuthData;
 
 public interface AuthDAO {
-    AuthData createAuth(String username) throws DataAccessException;
+    AuthData createAuth(String username);
 }
--- server/src/main/java/dataaccess/AuthMemoryDAO.java
+++ server/src/main/java/dataaccess/AuthMemoryDAO.java
@@ -5,12 +5,19 @@ import model.UserData;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 public class AuthMemoryDAO implements AuthDAO {
-    private Set<UserData> authData = new HashSet<>();
+    private Set<AuthData> authData = new HashSet<>();
 
-    public AuthData createAuth(String username) throws DataAccessException {
-        return null;
-        //TODO:: start here
+    public AuthData createAuth(String username) {
+        var authToken = generateToken();
+        AuthData newData = new AuthData(authToken, username);
+        authData.add(newData);
+        return newData;
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
     }
 }
