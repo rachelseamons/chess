88d700519bcd5c9f4485ef66175c4b6dbbb6798e
rachelseamons <rachelseamonshcp@gmail.com>
2024-12-04 20:11:59 -0700

    created ResponseException class

---
+++ shared/src/main/java/exception/ResponseException.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class ResponseException extends Exception {
+    final private int statusCode;
+
+    public ResponseException(int statusCode, String message) {
+        //sets the exception's message to the given message (from extended functionality)
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public int StatusCode() {
+        return statusCode;
+    }
+}
