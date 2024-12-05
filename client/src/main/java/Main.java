49861df32e1753d468c8e9355e1a82de8ac62c0c
rachelseamons <rachelseamonshcp@gmail.com>
2024-12-04 22:47:15 -0700

    succeeding "help" and "quit" commands

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,7 +4,6 @@ import ui.Repl;
 import java.util.Scanner;
 
 public class Main {
-    //TODO:: getting code for this from https://github.com/softwareconstruction240/softwareconstruction/blob/main/instruction/console-ui/console-ui.md
     public static void main(String[] args) {
         var serverUrl = "http://localhost:8080";
         if (args.length == 1) {
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -2,6 +2,8 @@ package ui;
 
 import java.util.Scanner;
 
+import static ui.EscapeSequences.*;
+
 public class Repl {
     private final ChessClient client;
 
@@ -12,6 +14,27 @@ public class Repl {
     public void run() {
         System.out.printf("Welcome to chess 240. Type \"Help\" to get started.%n");
         System.out.print(client.help());
-        System.out.printf("%n[LOGGED_OUT] >>>");
+        //TODO:: should I print the "help" list?
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                //TODO:: find a better green
+                System.out.print(SET_TEXT_COLOR_GREEN + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    public void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + "[LOGGED_OUT] >>> ");
     }
 }
